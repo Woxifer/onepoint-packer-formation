6594ba80ee755c41729d0cee2dae7ef75d8ca001
Pierre Sadki <p.sadki@groupeonepoint.com>
2025-01-08 13:00:59 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
+#!/bin/bash -eu
+
+set -x
+
 packer init aws-ebs.pkr.hcl
 packer validate aws-ebs.pkr.hcl
 packer build aws-ebs.pkr.hcl | tee packer_output.txt
-ami_id=$(< packer_output.txt tail | awk '$1=="eu-west"=:"{print $2}')
+ami_id=$(< packer_output.txt tail | awk '$1=="eu-west-1:"{print $2}')
 
 terraform -chdir=terraform init
 terraform -chdir=terraform validate
 terraform -chdir=terraform apply -var="ami_id=$ami_id" -auto-approve
-
+ip_address=$(terraform output -json | jq -r '.public_ip.value')
 
 curl "http://$ip_address/" | grep foo
 
